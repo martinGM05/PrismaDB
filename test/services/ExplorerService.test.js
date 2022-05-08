ca998ced996394aaa97837299ee6050ed273f0bd
Martin Gonzalez Miranda <mtn.gm05@gmail.com>
2022-05-08 13:46:04 -0500

    Add test for explorer service

---
+++ test/services/ExplorerService.test.js
@@ -0,0 +1,10 @@
+// const ExplorerService = require("../../lib/services/ExplorerService")
+
+describe("Test for Explorer Service", () => {
+    test("1) Test getAllExplorers", async () => {
+        // const explorers = await ExplorerService.getAllExplorers();
+        // expect(explorers.length).toBeGreaterThan(0);
+        const suma = 1 + 1;
+        expect(suma).toBe(5);
+    })
+})
