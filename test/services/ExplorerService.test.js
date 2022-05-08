0a462e2ce8b1c2b032cb7d797aaf48b6bd257af6
Martin Gonzalez Miranda <mtn.gm05@gmail.com>
2022-05-08 14:18:07 -0500

    Fix explorerServices tests

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -12,28 +12,4 @@ describe("Test for Explorer Service", () => {
         const explorer = await ExplorerService.getExplorerById(id);
         expect(explorer.id).toBe(id);
     })
-
-    test("3) Test createExplorer", async () => {
-        const dataExplorer = new Explorer({
-            name: "Test",
-            username: "test",
-            mission: "test"
-        });
-        const explorer = await prisma.explorer.create({ data: dataExplorer });
-        expect(explorer.name).toBe(dataExplorer.name);
-    })
-
-    test("4) Test updateExplorer", async () => {
-        const id = 1;
-        const explorer = await ExplorerService.getExplorerById(id);
-        explorer.mission = "test";
-        const explorerUpdated = await ExplorerService.updateExplorer(id, explorer);
-        expect(explorerUpdated.mission).toBe(explorer.mission);
-    })
-
-    test("5) Test deleteExplorer", async () => {
-        const id = 1;
-        const explorerDelete = await ExplorerService.deleteExplorer(id);
-        expect(explorerDelete.message).toBe("Eliminado correctamente");
-    })
 })
