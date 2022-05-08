d3ad38d49d69df47dc2ab6e5a91eb3530067a43b
Martin Gonzalez Miranda <mtn.gm05@gmail.com>
2022-05-08 14:32:16 -0500

    Fix Github Actions 4

--- prisma/seed.js
+++ prisma/seed.js
@@ -54,61 +54,6 @@ const prisma = new PrismaClient();
     })
 
     console.log('Create 3 explorers');
-
-    const Cwoopa = await prisma.comanders.upsert({
-      where: { name: 'Juan' },
-      update: {},
-      create: {
-        name: "Juan",
-        lang: "English",
-        missionCommander: "Rodrigo",
-        enrollments: 10,
-      },
-    });
-
-    const Cwoopa1 = await prisma.comanders.upsert({
-      where: { name: 'Ernesto' },
-      update: {},
-      create: {
-        name: "Ernesto",
-        lang: "English",
-        missionCommander: "Carlo",
-        enrollments: 50,
-      },
-    });
-
-    const Cwoopa2 = await prisma.comanders.upsert({
-      where: { name: 'Kevin' },
-      update: {},
-      create: {
-        name: "Kevin",
-        lang: "French",
-        missionCommander: "Fernanda",
-        enrollments: 30,
-      },
-    });
-
-    const Cwoopa3 = await prisma.comanders.upsert({
-      where: { name: 'Jose' },
-      update: {},
-      create: {
-        name: "Jose",
-        lang: "German",
-        missionCommander: "Carlo",
-        enrollments: 30,
-      },
-    });
-
-    const Cwoopa4 = await prisma.comanders.upsert({
-      where: { name: 'Jocelyn' },
-      update: {},
-      create: {
-        name: "Jocelyn",
-        lang: "German",
-        missionCommander: "Carlo",
-        enrollments: 30,
-      },
-    })
   } catch (e) {
     console.error(e);
     process.exit(1);
