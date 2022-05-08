ee76fee688fe6e6f2c92c7775de0a79b45d4bb25
Martin Gonzalez Miranda <mtn.gm05@gmail.com>
2022-05-08 13:47:58 -0500

    Add explorerService

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,10 +1,8 @@
-// const ExplorerService = require("../../lib/services/ExplorerService")
+const ExplorerService = require("../../lib/services/ExplorerService")
 
 describe("Test for Explorer Service", () => {
     test("1) Test getAllExplorers", async () => {
-        // const explorers = await ExplorerService.getAllExplorers();
-        // expect(explorers.length).toBeGreaterThan(0);
-        const suma = 1 + 1;
-        expect(suma).toBe(5);
+        const explorers = await ExplorerService.getAllExplorers();
+        expect(explorers.length).toBeGreaterThan(0);
     })
 })
